118d39bdb5c095ded691b945d5c2bb4aa9dfb389
NajmiFarhat <131953622+NajmiFarhat@users.noreply.github.com>
2025-05-15 22:08:03 +0800

    client.cpp

--- client.cpp
+++ client.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -14,52 +14,52 @@ int main(int argc, char *argv[]) {
     const char* server_ip = argv[1];
     int port = std::stoi(argv[2]);
 
-    // Create socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        std::cerr << "Error creating socket" << std::strerror(errno) << std::endl;
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
         return 1;
     }
 
-    // Server address information
+    // Create socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup server address
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
-    if (inet_pton(AF_INET, server_ip, &server_address.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
-        close(sock);
-        return 1;
-    }
+    inet_pton(AF_INET, server_ip, &server_address.sin_addr);
 
     // Connect to server
-    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        std::cerr << "Error connecting to server: " << std::strerror(errno) << std::endl;
-        close(sock);
+    if (connect(sock, (SOCKADDR*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
+        std::cerr << "Connection failed: " << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup();
         return 1;
     }
 
     std::cout << "Connected to server" << std::endl;
 
-    // Send data to server
     std::string message = "Hello from client!";
-    if (send(sock, message.c_str(), message.length(), 0) < 0) {
-        std::cerr << "Error sending data: " << std::strerror(errno) << std::endl;
-        close(sock);
-        return 1;
-    }
+    send(sock, message.c_str(), message.length(), 0);
     std::cout << "Sent message: " << message << std::endl;
 
-    // Receive response from server
-    char buffer[1024] = {0};
-    if (recv(sock, buffer, 1024, 0) < 0) {
-         std::cerr << "Error receiving data: " << std::strerror(errno) << std::endl;
-        close(sock);
-        return 1;
+    char buffer[1024] = {};
+    int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytesReceived > 0) {
+        buffer[bytesReceived] = '\0';  // Null-terminate
+        std::cout << "Received response: " << buffer << std::endl;
+    } else {
+        std::cerr << "Error receiving data\n";
     }
-    std::cout << "Received response: " << buffer << std::endl;
 
-    // Close socket
-    close(sock);
+    closesocket(sock);
+    WSACleanup();
     std::cout << "Connection closed" << std::endl;
 
     return 0;
